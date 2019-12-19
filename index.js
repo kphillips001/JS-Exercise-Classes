b40878c8763f6061b2e1533261c706c3c3ca694b
Kevin <knphillips01@gmail.com>
2019-12-19 14:27:42 -0600

    finished Task 3

--- index.js
+++ index.js
@@ -74,7 +74,21 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odoometer = 0;
+  }
+  fill(gallons) {
+    return this.tank += gallons;
+  }
+  drive(distance) {
+    this.odometer += distance; 
+    this.tank -= distance/this.milesPerGallon;
 
+    
+  }
 }
 
 /*
@@ -90,7 +104,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}. `
+  }
 }
 
 /*
